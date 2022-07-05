61572baa003d069b8fe70a6e7addc12a3ae92b19
bujin21 <cvj0810@naver.com>
2022-07-05 23:27:30 +0900

    복잡한 명령어 처리하기 위해, Rq 도입

--- src/com/jbj/exam/board/Main.java
+++ src/com/jbj/exam/board/Main.java
@@ -1,7 +1,9 @@
 package com.jbj.exam.board;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -17,6 +19,7 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
+
 		System.out.println("== 게시판 v 0.1 ==");
 		System.out.println("== 프로그램 시작 ==");
 		
@@ -35,10 +38,12 @@ public class Main {
 			System.out.printf("명령)");
 			String cmd = sc.nextLine();
 			
-			if(cmd.equals("exit")) {
+			Rq rq = new Rq(cmd);
+			
+			if(rq.getUrlPath().equals("exit")) {
 				break;
 				
-			}else if ( cmd.equals("/usr/article/list")) {
+			}else if (rq.getUrlPath().equals("/usr/article/list")) {
 				System.out.println("- 게시물 리스트 -");
 				System.out.println("-----------------");
 				System.out.println("번호 / 제목");
@@ -51,7 +56,7 @@ public class Main {
 
 			
 			// /usr/article/detail 입력되면 가장 최근 게시물 정보 노출
-			}else if ( cmd.equals("/usr/article/detail")) {
+			}else if ( rq.getUrlPath().equals("/usr/article/detail")) {
 				
 
 				if ( articles.isEmpty()) {
@@ -66,7 +71,7 @@ public class Main {
 				System.out.printf("제목 : %s\n", article.title);
 				System.out.printf("내용 : %s\n", article.body);
 				
-			}else if(cmd.equals("/usr/article/write")) {
+			}else if(rq.getUrlPath().equals("/usr/article/write")) {
 				System.out.println("- 게시물 등록 -");
 				System.out.printf("제목 :");
 				String title = sc.nextLine();
@@ -94,6 +99,54 @@ public class Main {
 	}
 
 }
+class Rq {
+	private String url; // 접근제어자를 붙이는게 관례. 외부에서 접근 불가능.
+	private String urlPath;
+	private Map<String, String> params;
+	// 인스턴스 변수 -> 여기에 다 붙임
+
+	Rq(String url) {
+		this.url = url;
+		urlPath = Util.getUrlPathFromUrl(this.url);
+		params = Util.getParamsFromUrl(this.url);
+	}
+
+	public Map<String, String> getParams() {
+		return params;
+	}
+
+	public String getUrlPath() {
+		return urlPath;
+	}
+}
+
+
+class Util {
+	static Map<String, String> getParamsFromUrl(String url) {
+		Map<String, String> params = new HashMap<>();
+		String[] urlBits = url.split("\\?", 2);
+
+		if (urlBits.length == 1) {
+			return params;
+		}
+
+		String queryStr = urlBits[1];
+		for (String bit : queryStr.split("&")) {
+			String[] bits = bit.split("=", 2);
+			if (bits.length == 1) {
+				continue;
+			}
+			params.put(bits[0], bits[1]);
+		}
+
+		return params;
+	}
+
+	static String getUrlPathFromUrl(String url) {
+		return url.split("\\?", 2)[0];
+	}
+}
+
 class Article{
 	
 	int id;
